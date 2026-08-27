dc699469bb5a5d42a359bd2ac5f401d67c9e5388
Kingblaq7 <166668343+Kingblaq7@users.noreply.github.com>
2026-08-27 14:29:25 +0100

    Create chains.ts

---
+++ lib/chains.ts
@@ -0,0 +1,37 @@
+export const CHAINS = [
+  {
+    id: 1,
+    name: "Ethereum",
+    symbol: "ETH",
+  },
+  {
+    id: 56,
+    name: "BNB Chain",
+    symbol: "BNB",
+  },
+  {
+    id: 137,
+    name: "Polygon",
+    symbol: "MATIC",
+  },
+  {
+    id: 42161,
+    name: "Arbitrum",
+    symbol: "ETH",
+  },
+  {
+    id: 10,
+    name: "Optimism",
+    symbol: "ETH",
+  },
+  {
+    id: 8453,
+    name: "Base",
+    symbol: "ETH",
+  },
+  {
+    id: 43114,
+    name: "Avalanche",
+    symbol: "AVAX",
+  },
+];
